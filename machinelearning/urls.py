f8b60780858619d8220e834a1ae2238ebd813316
bkwapong <benjamin.kwapong@gmail.com>
2020-03-26 10:40:04 -0400

    Modified machinelearning/urls.py

--- machinelearning/urls.py
+++ machinelearning/urls.py
@@ -22,5 +22,8 @@ urlpatterns = [
 	path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
 	path('dashboard/', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
+	path('matrix_factorization/', TemplateView.as_view(template_name='MF.html'), name='Matrix_Factorization'),
+	path('neural_network/', TemplateView.as_view(template_name='NN.html'), name='Neural_Network'),
+	path('scratch_pad/', TemplateView.as_view(template_name='SP.html'), name='Scratch_Pad'),
 	path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
