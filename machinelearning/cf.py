6725bb9145fe4656acd4000a6e8383fdc796a1d3
ashleyfarrell85991 <ashleymfarrell85@Gmail.com>
2020-05-05 19:24:00 -0400

    -cleanup
    Signed-off-by: ashleyfarrell85991 <ashleymfarrell85@Gmail.com>

--- machinelearning/cf.py
+++ machinelearning/cf.py
@@ -4,9 +4,6 @@ import random
 import sys
 from operator import itemgetter
 
-import matplotlib.pyplot as plt
-import plotly.offline as py
-import plotly.tools as tls
 from surprise import Reader
 
 random.seed(0)
@@ -174,122 +171,6 @@ class UserBasedCF:
                (precision, recall, coverage), file=outfile)
         print ('precision=%.4f\nrecall=%.4f\ncoverage=%.4f' %
                (precision, recall, coverage), file=graphfile)
-
-
-
-
-
-
-def get_top_n(predictions, n=30):
-    # First map the predictions to each user.
-    top_n = defaultdict(list)
-    for uid, iid, true_r, est, _ in predictions:
-        top_n[uid].append((iid, est))
-
-    # Then sort the predictions for each user and retrieve the k highest ones.
-    for uid, user_ratings in top_n.items():
-        user_ratings.sort(key=lambda x: x[1], reverse=True)
-        top_n[uid] = user_ratings[:n]
-
-    return top_n
-
-
-def predictions(data, rec_inp=30, split_inp=.3):
-    trainset, testset = train_test_split(data, test_size=split_inp)
-    algo = SVD()
-    algo.fit(trainset)
-    predictions = algo.test(testset)
-    top_n = get_top_n(predictions, n=rec_inp)
-    for uid, user_ratings in top_n.items():
-        uid, [iid for (iid, _) in user_ratings]
-        return predictions
-
-
-from collections import defaultdict
-
-from surprise import Dataset
-from surprise import SVD
-from surprise.model_selection import KFold, train_test_split
-
-
-def precision_recall_at_k(predictions, k=30, threshold=3.5):
-    '''Return precision and recall at k metrics for each user.'''
-
-    # First map the predictions to each user.
-    user_est_true = defaultdict(list)
-    for uid, _, true_r, est, _ in predictions:
-        user_est_true[uid].append((est, true_r))
-
-    precisions = dict()
-    recalls = dict()
-    for uid, user_ratings in user_est_true.items():
-        # Sort user ratings by estimated value
-        user_ratings.sort(key=lambda x: x[0], reverse=True)
-
-        # Number of relevant items
-        n_rel = sum((true_r >= threshold) for (_, true_r) in user_ratings)
-
-        # Number of recommended items in top k
-        n_rec_k = sum((est >= threshold) for (est, _) in user_ratings[:k])
-
-        # Number of relevant and recommended items in top k
-        n_rel_and_rec_k = sum(((true_r >= threshold) and (est >= threshold))
-                              for (est, true_r) in user_ratings[:k])
-
-        # Precision@K: Proportion of recommended items that are relevant
-        # When n_rec_k is 0, Precision is undefined. We here set it to 0.
-
-        precisions[uid] = n_rel_and_rec_k / n_rec_k if n_rec_k != 0 else 0
-
-        # Recall@K: Proportion of relevant items that are recommended
-        # When n_rel is 0, Recall is undefined. We here set it to 0.
-
-        recalls[uid] = n_rel_and_rec_k / n_rel if n_rel != 0 else 0
-
-    return precisions, recalls
-
-
-def generate_graph(data, user_inp):
-    kf = KFold(n_splits=5)
-    algo = SVD()
-    for trainset, testset in kf.split(data):
-        algo.fit(trainset)
-        predictions = algo.test(testset)
-        # precisions, recalls = precision_recall_at_k(predictions, k=5, threshold=4)
-        results = []
-        for i in range(2, user_inp):
-            precisions, recalls = precision_recall_at_k(predictions, k=i, threshold=2.5)
-
-            # Precision and recall can then be averaged over all users
-            prec = sum(prec for prec in precisions.values()) / len(precisions)
-            rec = sum(rec for rec in recalls.values()) / len(recalls)
-            results.append({'K': i, 'Precision': prec, 'Recall': rec})
-    Rec = []
-    Precision = []
-    Recall = []
-    for i in range(0, user_inp):
-        Rec.append(results[i]['K'])
-        Precision.append(results[i]['Precision'])
-        Recall.append(results[i]['Recall'])
-    fig = plt.figure()
-    plt.plot(Rec, Precision)
-    plt.xlabel('# of Recommendations')
-    plt.ylabel('Precision')
-    plt2 = plt.twinx()
-    plt2.plot(Rec, Recall, 'r')
-    plt.ylabel('Recall')
-    for tl in plt2.get_yticklabels():
-        tl.set_color('r')
-    plt.show()
-    plt.savefig('./recallprecision.png')
-    fig = plt.Figure()
-    ax = fig.gca()
-    ax2 = ax.twinx()
-    ax2.plot(Rec,Precision)
-    ax.plot(Rec,Precision)
-   # canvas = FigureCanvas(fig)
-    plotly_fig = tls.mpl_to_plotly(fig)
-    py.plot(plotly_fig, filename='Precision')
 
 
 
